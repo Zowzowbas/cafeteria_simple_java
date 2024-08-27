7fad12883d3fcbfaa27189f0dd26ce6bf4963217
Zowzowbas <160196974+Zowzowbas@users.noreply.github.com>
2024-08-27 10:22:37 +0800

    Add files via upload

--- computeprojz/src/main/java/com/mycompany/computeprojz/Computeprojz.java
+++ computeprojz/src/main/java/com/mycompany/computeprojz/Computeprojz.java
@@ -73,6 +73,104 @@ public class Computeprojz {
                                             
                                         }
                             }
+                            
+                            else if (q4 == 2)
+                            {
+                                System.out.println("Water (15 pesos)");
+                                int total;
+                                total = piatos + water;
+                                System.out.println("Your Total is: " + total);
+                                System.out.println("Enter cash: ");
+                                int cash = input.nextInt();
+                                
+                                    while (cash < total)
+                                    {
+                                        System.out.println("Insufficient Cash!");
+                                        System.out.println("Enter cash: ");
+                                        cash = input.nextInt();
+                                    }
+                                    
+                                        if (cash >= total)
+                                        {
+                                            int change;
+                                            change = cash - total;
+                                            System.out.println("Thank you for buying on our store :)");
+                                            System.out.println("Your total change is: " + change);
+                                            
+                                        }
+                            }
+                        }
+                }
+                
+                else if (q2 == 2)
+                {
+                    System.out.println("Nova (25 pesos)");
+                    
+                    System.out.println("Would you like to order drinks? [1] - Yes / [2] - No:");
+                    int q3 = input.nextInt();
+                    
+                        if (q3 == 1)
+                        {
+                            System.out.println("Drinks");
+                            System.out.println("[1] - Coke (20 pesos)");
+                            System.out.println("[2] - Water (15 pesos)");
+                            System.out.println("Enter the drinks of your choice: ");
+                            int q4 = input.nextInt();
+                            int coke, water;
+                            coke = 20;
+                            water = 15;
+                            
+                            if (q4 == 1)
+                            {
+                                System.out.println("Coke (20 pesos)");
+                                int total;
+                                total = nova + coke;
+                                System.out.println("Your Total is: " + total);
+                                System.out.println("Enter cash: ");
+                                int cash = input.nextInt();
+                                
+                                    while (cash < total)
+                                    {
+                                        System.out.println("Insufficient Cash!");
+                                        System.out.println("Enter cash: ");
+                                        cash = input.nextInt();
+                                    }
+                                    
+                                        if (cash >= total)
+                                        {
+                                            int change;
+                                            change = cash - total;
+                                            System.out.println("Thank you for buying on our store :)");
+                                            System.out.println("Your total change is: " + change);
+                                            
+                                        }
+                            }
+                            else if (q4 == 2)
+                            {
+                                System.out.println("Water (15 pesos)");
+                                int total;
+                                total = nova + water;
+                                System.out.println("Your Total is: " + total);
+                                System.out.println("Enter cash: ");
+                                int cash = input.nextInt();
+                                
+                                    while (cash < total)
+                                    {
+                                        System.out.println("Insufficient Cash!");
+                                        System.out.println("Enter cash: ");
+                                        cash = input.nextInt();
+                                    }
+                                    
+                                        if (cash >= total)
+                                        {
+                                            int change;
+                                            change = cash - total;
+                                            System.out.println("Thank you for buying on our store :)");
+                                            System.out.println("Your total change is: " + change);
+                                            
+                                        }
+                            }
+                            
                         }
                 }
         }
@@ -100,7 +198,7 @@ public class Computeprojz {
                             System.out.println("Snacks");
                             System.out.println("[1] - Piatos (20 pesos)");
                             System.out.println("[2] - Nova (25 pesos)");
-                            System.out.println("Enter the snack of your choice: ");
+                            System.out.println("Enter the snacks of your choice: ");
                             int q4 = input.nextInt();
                             int piatos, nova;
                             piatos = 20;
@@ -109,9 +207,128 @@ public class Computeprojz {
                             if (q4 == 1)
                             {
                                 System.out.println("Piatos (20 pesos)");
+                                int total;
+                                total = coke + piatos;
+                                System.out.println("Your Total is: " + total);
+                                System.out.println("Enter cash: ");
+                                int cash = input.nextInt();
+                                
+                                    while (cash < total)
+                                    {
+                                        System.out.println("Insufficient Cash!");
+                                        System.out.println("Enter cash: ");
+                                        cash = input.nextInt();
+                                    }
+                                    
+                                        if (cash >= total)
+                                        {
+                                            int change;
+                                            change = cash - total;
+                                            System.out.println("Thank you for buying on our store :)");
+                                            System.out.println("Your total change is: " + change);
+                                            
+                                        }
+                            }
+                            
+                            else if (q4 == 2)
+                            {
+                                System.out.println("Nova (25 pesos)");
+                                int total;
+                                total = coke + nova;
+                                System.out.println("Your Total is: " + total);
+                                System.out.println("Enter cash: ");
+                                int cash = input.nextInt();
+                                
+                                    while (cash < total)
+                                    {
+                                        System.out.println("Insufficient Cash!");
+                                        System.out.println("Enter cash: ");
+                                        cash = input.nextInt();
+                                    }
+                                    
+                                        if (cash >= total)
+                                        {
+                                            int change;
+                                            change = cash - total;
+                                            System.out.println("Thank you for buying on our store :)");
+                                            System.out.println("Your total change is: " + change);
+                                            
+                                        }
                             }
                         }
-                }     
+                }
+                
+                else if (q2 == 2)
+                {
+                    System.out.println("Water (15 pesos)");
+                    
+                    System.out.println("Would you like to order snacks? [1] - Yes / [2] - No:");
+                    int q3 = input.nextInt();
+                    
+                        if (q3 == 1)
+                        {
+                            System.out.println("Snacks");
+                            System.out.println("[1] - Piatos (20 pesos)");
+                            System.out.println("[2] - Nova (25 pesos)");
+                            System.out.println("Enter the snacks of your choice: ");
+                            int q4 = input.nextInt();
+                            int piatos, nova;
+                            piatos = 20;
+                            nova = 25;
+                            
+                            if (q4 == 1)
+                            {
+                                System.out.println("Piatos (20 pesos)");
+                                int total;
+                                total = water + piatos;
+                                System.out.println("Your Total is: " + total);
+                                System.out.println("Enter cash: ");
+                                int cash = input.nextInt();
+                                
+                                    while (cash < total)
+                                    {
+                                        System.out.println("Insufficient Cash!");
+                                        System.out.println("Enter cash: ");
+                                        cash = input.nextInt();
+                                    }
+                                    
+                                        if (cash >= total)
+                                        {
+                                            int change;
+                                            change = cash - total;
+                                            System.out.println("Thank you for buying on our store :)");
+                                            System.out.println("Your total change is: " + change);
+                                            
+                                        }
+                            }
+                            else if (q4 == 2)
+                            {
+                                System.out.println("Nova (25 pesos)");
+                                int total;
+                                total = nova + water;
+                                System.out.println("Your Total is: " + total);
+                                System.out.println("Enter cash: ");
+                                int cash = input.nextInt();
+                                
+                                    while (cash < total)
+                                    {
+                                        System.out.println("Insufficient Cash!");
+                                        System.out.println("Enter cash: ");
+                                        cash = input.nextInt();
+                                    }
+                                    
+                                        if (cash >= total)
+                                        {
+                                            int change;
+                                            change = cash - total;
+                                            System.out.println("Thank you for buying on our store :)");
+                                            System.out.println("Your total change is: " + change);
+                                            
+                                        }
+                            }
+                            
+                        }
+                }
         }
         
         else 
